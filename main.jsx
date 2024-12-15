8ed410d3e21b3015b0a699c909cb4ce2b67db5be
Hoa Chau <hoachau1010@gmail.com>
2024-12-15 21:39:01 +0000

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Header from "./Header.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header/>
+  </React.StrictMode>
+);
+
